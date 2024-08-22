75112a6b2f1f3d8aae09ff2b9e87bbdd842010c8
Andrew Ross <5852283+andrew-c-ross@users.noreply.github.com>
2024-08-22 15:03:08 -0400

    Clarify which diagnostics are for concentration

--- src/tracer/MOM_tracer_registry.F90
+++ src/tracer/MOM_tracer_registry.F90
@@ -456,19 +456,19 @@ subroutine register_tracer_diagnostics(Reg, h, Time, diag, G, GV, US, use_ALE, u
         trim(lowercase(flux_longname)), conv_units, v_extensive=.true., &
         conversion=Tr%conv_scale*US%s_to_T)
     Tr%id_difc_xy = register_diag_field('ocean_model',trim(shortnm)//"_diffusionc_xy", &
-        diag%axesTL, Time, "Horizontal convergence of residual mean diffusive fluxes of "//trim(shortnm), &
-        trim(units)//' s-1')
+        diag%axesTL, Time, "Horizontal convergence of residual mean diffusive fluxes of "//&
+        trim(shortnm)//' concentration', trim(units)//' s-1')
     Tr%id_dif_xy = register_diag_field('ocean_model',trim(shortnm)//"_diffusion_xy", &
         diag%axesTL, Time, "Horizontal convergence of residual mean diffusive fluxes of "//trim(shortnm), &
         trim(units)//' s-1')
     Tr%id_advc_xy = register_diag_field('ocean_model',trim(shortnm)//"_advectionc_xy", &
-        diag%axesTL, Time, "Horizontal convergence of residual mean advective fluxes of "//trim(shortnm), &
-        trim(units)//' s-1')
+        diag%axesTL, Time, "Horizontal convergence of residual mean advective fluxes of "//&
+        trim(shortnm)//' concentration', trim(units)//' s-1')
     Tr%id_advc_x  = register_diag_field("ocean_model",trim(shortnm)//'_advectionc_x', &
-        diag%axesTL, Time, "Horizontal x mean advective fluxes of "//trim(shortnm), &
+        diag%axesTL, Time, "Horizontal x mean advective fluxes of "//trim(shortnm)//' concentration', &
         trim(units)//' s-1')
     Tr%id_advc_y  = register_diag_field("ocean_model",trim(shortnm)//'_advectionc_y', &
-        diag%axesTL, Time, "Horizontal y mean advective fluxes of "//trim(shortnm), &
+        diag%axesTL, Time, "Horizontal y mean advective fluxes of "//trim(shortnm)//' concentration', &
         trim(units)//' s-1')
     Tr%id_adv_xy_2d = register_diag_field('ocean_model', trim(shortnm)//"_advection_xy_2d", &
         diag%axesT1, Time, &
